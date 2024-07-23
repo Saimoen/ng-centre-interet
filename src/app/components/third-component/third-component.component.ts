adaa73a8ffb5614eb8bc06b2f505ba9ecd0aa8f0
Saimoen <gregorysaimoen@outlook.com>
2024-07-24 10:11:02 +1100

    photographie terminé

--- src/app/components/third-component/third-component.component.ts
+++ src/app/components/third-component/third-component.component.ts
@@ -1,12 +1,34 @@
+import { photo } from './../../shared/assets/data/data';
 import { Component } from '@angular/core';
+import * as data from '../../shared/assets/data/data';
+import { NgFor } from '@angular/common';
+
+interface photo {
+  emplacement_img: string;
+  cliches: {
+    titre: string;
+    date: string;
+    fichier: string;
+    description: string;
+  }[];
+}
 
 @Component({
   selector: 'app-third-component',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './third-component.component.html',
   styleUrl: './third-component.component.css'
 })
 export class ThirdComponentComponent {
+
+  photos: any[] = [];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.photos = [data.photo.cliches];
+    console.log(this.photos);
+  }
 
 }
